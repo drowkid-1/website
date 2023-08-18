49ead9fabfd11354debcd9f0b82fb572879ee1a7
DrowKid <drowkid01@gmail.com>
2023-08-18 14:02:34 -0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const { awesome } = useAppConfig()
+definePageMeta({ layout: 'page' })
+useHead({
+  titleTemplate: '',
+  title: awesome?.name || 'Bienvenidxs a DrowKid Web',
+})
+</script>
+
+<template>
+  <AwesomeWelcome :with-alert="true" />
+</template>
